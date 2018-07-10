2ae7998243f50a2cc87d9e89bf44ca17d1c059b4
lyfeyaj <lyfeyaj@gmail.com>
2018-07-10 20:38:42 +0800

    optimize cli info

--- packages/cli/lib/cli.js
+++ packages/cli/lib/cli.js
@@ -3,26 +3,33 @@
 'use strict';
 
 require('yargs')
-  .command('new [dir]', 'start the server', (yargs) => {
+  .command('new [projectName]', '创建新的微信小程序项目', (yargs) => {
     yargs
-      .positional('dir', {
-        describe: 'directory to initialize new wechat app program',
+      .positional('projectName', {
+        describe: '项目名称',
         default: '.'
       });
   }, (argv) => {
     require('./commands/create')(argv);
   })
-  .command('init', 'init wechat app project', {}, (argv) => {
+  .command('init', '在现有的小程序项目中初始化 EWA', {}, (argv) => {
     require('./commands/init')(argv);
   })
-  .command('start', 'start ewa build server', {}, (argv) => {
+  .command('start', '启动 EWA 小程序项目实时编译', {}, (argv) => {
     require('./commands/start')(argv);
   })
-  .command('build', 'build static files', {}, (argv) => {
+  .command('build', '编译小程序静态文件', {}, (argv) => {
     require('./commands/build')(argv);
   })
-  .command('clean', 'clean dist files', {}, (argv) => {
+  .command('clean', '清理小程序静态文件', {}, (argv) => {
     require('./commands/clean')(argv);
   })
-  .help()
+  .options({
+    version: {
+      alias: 'v',
+      describe: '当前版本号'
+    }
+  })
+  .help('help', '获取使用帮助')
+  .alias('help', 'h')
   .argv;
