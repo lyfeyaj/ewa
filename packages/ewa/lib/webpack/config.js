2584513339f726335403b005880a9b638cf45414
lyfeyaj <lyfeyaj@gmail.com>
2018-07-10 15:36:56 +0800

    update config

--- packages/ewa/lib/webpack/config.js
+++ packages/ewa/lib/webpack/config.js
@@ -16,12 +16,4 @@ if (fs.existsSync(USER_CONFIG_FILE)) {
   config = require(USER_CONFIG_FILE);
 }
 
-// webpack(makeConfig(config), (err, stats) => {
-//   if (err || stats.hasErrors()) {
-//     // 在这里处理错误
-//     console.log(err, stats);
-//   }
-//   // 处理完成
-// });
-
 module.exports = makeConfig(config);
