8be62efcbfb7d7b3fa98d410b1cd84ffb2f8bc93
lyfeyaj <lyfeyaj@gmail.com>
2018-07-11 13:41:35 +0800

    remove unused codes

--- packages/ewa/lib/webpack/config.js
+++ packages/ewa/lib/webpack/config.js
@@ -2,7 +2,6 @@
 
 const fs = require('fs');
 const path = require('path');
-// const webpack = require('webpack');
 const makeConfig = require('./makeConfig');
 
 const USER_CONFIG_FILE = path.join(
