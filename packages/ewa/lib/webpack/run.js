f9528ce742ea292d04a55145de377aaf0bc1f7e9
lyfeyaj <lyfeyaj@gmail.com>
2018-09-20 11:25:07 +0800

    add windows support

--- packages/ewa/lib/webpack/run.js
+++ packages/ewa/lib/webpack/run.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const path = require('path');
+
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 let webpackBin = require.resolve('webpack').replace(
-  'webpack/lib/webpack.js',
-  '.bin/webpack'
+  path.normalize('webpack/lib/webpack.js'),
+  path.normalize('.bin/webpack')
 );
 
 if (NODE_ENV === 'development') {
