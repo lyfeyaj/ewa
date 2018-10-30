5f800756cdc77e891cfa1135527d4f206f535d69
lyfeyaj <lyfeyaj@gmail.com>
2018-10-30 13:53:55 +0800

    add less support and upgrade babel to 7

--- packages/ewa/lib/webpack/makeConfig.js
+++ packages/ewa/lib/webpack/makeConfig.js
@@ -35,6 +35,7 @@ const DEFAULT_COPY_FILE_TYPES = [
   'ico'
 ];
 const DEFAULT_COMMON_MODULE_PATTERN = /[\\/](node_modules|utils|vendor)[\\/].+\.js/;
+const DEFAULT_CSS_PARSER = 'sass';
 
 /**
  * 生成 webpack 配置
@@ -47,6 +48,7 @@ const DEFAULT_COMMON_MODULE_PATTERN = /[\\/](node_modules|utils|vendor)[\\/].+\.
  *   rules: webpack loader 规则
  *   plugins: webpack plugin
  *   autoCleanUnusedFiles: 开发环境下是否自动清理无用文件，默认为 true
+ *   cssParser: sass 或者 less，默认为 sass
  *   webpack: 修改并自定义 webpack 配置，如：function(config) { return config; }
  */
 module.exports = function makeConfig(options = {}) {
@@ -54,7 +56,8 @@ module.exports = function makeConfig(options = {}) {
     commonModuleName: DEFAULT_COMMON_MODULE_NAME,
     commonModulePattern: DEFAULT_COMMON_MODULE_PATTERN,
     aliasDirs: DEFAULT_ALIAS_DIRS,
-    copyFileTypes: DEFAULT_COPY_FILE_TYPES
+    copyFileTypes: DEFAULT_COPY_FILE_TYPES,
+    cssParser: DEFAULT_CSS_PARSER
   }, options);
 
   options.simplifyPath = options.simplifyPath === true;
@@ -176,7 +179,7 @@ module.exports = function makeConfig(options = {}) {
   let rules = [];
 
   if (IS_DEV) {
-    // do nothing
+    // 开发环境下增加 eslint 检查
     rules.push(
       {
         enforce: 'pre',
@@ -185,6 +188,7 @@ module.exports = function makeConfig(options = {}) {
         use: [{
           loader: 'eslint-loader',
           options: {
+            cache: true,
             fix: true
           }
         }]
@@ -192,6 +196,7 @@ module.exports = function makeConfig(options = {}) {
     );
   }
 
+  // 不同文件类型的处理
   rules = rules.concat([
     // 解析 js 文件
     {
@@ -200,77 +205,36 @@ module.exports = function makeConfig(options = {}) {
         loader: 'babel-loader',
         options: {
           cacheDirectory: true,
-          presets: [
-            require('babel-preset-env'),
-            require('babel-preset-es2016'),
-            require('babel-preset-es2017'),
-            require('babel-preset-stage-2')
-          ],
+          // 让 babel 根据文件判断是 commonjs 或者 esmodule
+          sourceType: 'unambiguous',
+          presets: [[require('@babel/preset-env'), { targets: { ios: '7' } }]],
           plugins: [
-            [require('babel-plugin-transform-runtime'), {
-              'helpers': false,
-              'polyfill': true,
-              'regenerator': true,
-              'moduleName': 'babel-runtime'
-            }]
+            [
+              require('@babel/plugin-transform-runtime'),
+              {
+                helpers: true,
+                corejs: false,
+                regenerator: true
+              }
+            ],
+            require('@babel/plugin-proposal-class-properties'),
+            [
+              require('@babel/plugin-proposal-decorators'),
+              { decoratorsBeforeExport: true }
+            ],
+            require('@babel/plugin-proposal-export-namespace-from'),
+            require('@babel/plugin-proposal-function-sent'),
+            require('@babel/plugin-proposal-json-strings'),
+            require('@babel/plugin-proposal-numeric-separator'),
+            require('@babel/plugin-proposal-throw-expressions'),
+            require('@babel/plugin-syntax-dynamic-import'),
+            require('@babel/plugin-syntax-import-meta'),
+            require('@babel/plugin-transform-async-to-generator'),
+            require('@babel/plugin-transform-regenerator'),
           ]
         }
       }],
       exclude: /node_modules/
-    },
-
-    // 解析并抽离 wxss 文件
-    {
-      test: /\.(css|scss|sass|wxss)$/,
-      use:   ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          {
-            loader: 'css-loader',
-            options: { minimize: !IS_DEV, import: false }
-          },
-          // PostCSS 配置
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: function() {
-                let ps = [
-                  require('autoprefixer')({
-                    remove: false,
-                    browsers: ['iOS 7']
-                  })
-                ];
-
-                if (!IS_DEV) {
-                  ps = ps.concat(
-                    require('cssnano')({
-                      preset: [
-                        'default',
-                        {
-                          discardComments: { removeAll: true },
-                          calc: false
-                        }
-                      ]
-                    })
-                  );
-                }
-                return ps;
-              },
-              sourceMap: 'inline'
-            }
-          },
-          { loader: 'resolve-url-loader' },
-          { loader: './loaders/fix-import-wxss-loader.js' },
-          {
-            loader: 'sass-loader',
-            options: {
-              // resolve-url-loader 需要开启 sourceMap 才能工作
-              sourceMap: true
-            }
-          },
-          { loader: './loaders/import-wxss-loader.js' },
-        ]
-      })
     },
 
     // 处理图片, 小程序不支持本地的背景图片, 这里采用 base64 编码的 datauri
@@ -314,6 +278,79 @@ module.exports = function makeConfig(options = {}) {
     }
   ]);
 
+  // 解析并抽离 wxss 文件
+  let cssPattern;
+  let cssRules = [];
+  let cssExtensions = ['.css', '.wxss'];
+  if (options.cssParser === 'sass') {
+    cssPattern = /\.(css|scss|sass|wxss)$/;
+    cssRules = [
+      { loader: 'resolve-url-loader' },
+      { loader: './loaders/fix-import-wxss-loader.js' },
+      {
+        loader: 'sass-loader',
+        options: {
+          // resolve-url-loader 需要开启 sourceMap 才能工作
+          sourceMap: true
+        }
+      },
+      { loader: './loaders/import-wxss-loader.js' }
+    ];
+    cssExtensions = cssExtensions.concat(['.scss', '.sass']);
+  } else if (cssPattern === 'less') {
+    cssPattern = /\.(css|less|wxss)$/;
+    cssRules = [
+      { loader: './loaders/fix-import-wxss-loader.js' },
+      { loader: 'less-loader', options: { sourceMap: true } },
+      { loader: './loaders/import-wxss-loader.js' }
+    ];
+    cssExtensions = cssExtensions.concat(['.less']);
+  }
+
+  rules.push({
+    test: cssPattern,
+    use: ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: [{
+        loader: 'css-loader',
+        options: {
+          minimize: !IS_DEV,
+          // 不处理 css 的 @import, 充分利用 wxss 本身的 @import
+          import: false
+        }
+      },
+      // PostCSS 配置
+      {
+        loader: 'postcss-loader',
+        options: {
+          plugins: function() {
+            let p = [
+              require('autoprefixer')({ remove: false, browsers: ['iOS 7']})
+            ];
+
+            if (!IS_DEV) {
+              p = p.concat(
+                require('cssnano')({
+                  preset: [
+                    'default',
+                    {
+                      discardComments: { removeAll: true },
+                      // calc 无法计算 rpx，此处禁止
+                      calc: false
+                    }
+                  ]
+                })
+              );
+            }
+
+            return p;
+          },
+          sourceMap: 'inline'
+        }
+      }].concat(cssRules)
+    })
+  });
+
   // 构建优化
   const optimization = {
     splitChunks: {
@@ -354,7 +391,7 @@ module.exports = function makeConfig(options = {}) {
         'node_modules',
         path.resolve(__dirname, '../../node_modules')
       ],
-      extensions: ['.js', '.html', '.css', '.scss', '.sass', '.wxml', '.wxss', '.wxs'],
+      extensions: ['.js', '.html', '.wxml', '.wxs'].concat(cssExtensions),
       alias: Object.assign(aliasDirs, {
         '@': path.resolve(ROOT, 'src/')
       })
