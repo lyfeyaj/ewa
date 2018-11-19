1102b067d9f5d9144055d06860f8477b4ce9316b
lyfeyaj <lyfeyaj@gmail.com>
2018-11-19 18:36:59 +0800

    optmize module cache

--- packages/ewa/lib/webpack/plugins/NodeCommonModuleTemplatePlugin.js
+++ packages/ewa/lib/webpack/plugins/NodeCommonModuleTemplatePlugin.js
@@ -6,6 +6,8 @@ const path = require('path');
 // Constants
 const DEFAULT_COMMON_MODULE_NAME = 'vendors.js';
 
+const GLOBAL_CACHED_VAR = 'global.__cim';
+
 // Inject common module into entry files
 module.exports = class NodeCommonModuleTemplatePlugin {
   constructor(options = {}) {
@@ -17,6 +19,7 @@ module.exports = class NodeCommonModuleTemplatePlugin {
   apply(compiler) {
     compiler.hooks.thisCompilation.tap('NodeCommonModuleTemplatePlugin', compilation => {
       const mainTemplate = compilation.mainTemplate;
+
       mainTemplate.hooks.bootstrap.tap('NodeCommonModuleTemplatePlugin', (source, chunk) => {
         const vendorPath = path.relative(
           path.dirname(chunk.name),
@@ -30,14 +33,28 @@ module.exports = class NodeCommonModuleTemplatePlugin {
           '(function loadVendorModules() {',
           Template.indent([
             `var vendors = require('${vendorPath}');`,
-            'var extraModules = vendors.modules || {};',
-            'for (var name in extraModules) {',
+            'var extraModules = { __proto__: vendors.modules || {} };',
+            'for (var name in modules) {',
             Template.indent([
-              'modules[name] = extraModules[name];'
+              'if (!extraModules[name]) extraModules[name] = modules[name];'
             ]),
-            '}'
+            '}',
+            'modules = extraModules;'
           ]),
-          '})();'
+          '})();',
+
+          // Add global cachedInstalledModules
+          '',
+          `${GLOBAL_CACHED_VAR} = ${GLOBAL_CACHED_VAR} || {};`
+        ]);
+      });
+
+      // cachedInstalledModules => __cim
+      mainTemplate.hooks.localVars.tap('NodeCommonModuleTemplatePlugin', (source) => {
+        return Template.asString([
+          source,
+          '// replace with global cache',
+          `installedModules = ${GLOBAL_CACHED_VAR} || {};`
         ]);
       });
     });
