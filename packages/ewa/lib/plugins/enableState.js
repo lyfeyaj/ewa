8af3e44fbd809fbe47ba0018b0315e393b409c84
lyfeyaj <lyfeyaj@gmail.com>
2019-09-11 14:48:16 +0800

    Plugin - enableState: fix state diff bug and optimize debug info

--- packages/ewa/lib/plugins/enableState.js
+++ packages/ewa/lib/plugins/enableState.js
@@ -7,18 +7,20 @@ const cloneDeep = require('lodash.clonedeep');
 const pick = require('lodash.pick');
 const keys = require('lodash.keys');
 
-const noop = function () {};
+const noop = function(){};
 
 // 日志打印
-const logger = function(type, name, changes) {
+const logger = function(type, name, timeConsumption = 0, changes) {
   if (!console) return;
+  let timeConsumptionMsg = `Diff 耗时: ${timeConsumption}ms`;
   try {
     if (console.group) {
       console.group(type, name);
+      console.log(timeConsumptionMsg);
       console.log(changes);
       console.groupEnd();
     } else {
-      console.log(type, name, changes);
+      console.log(type, name, timeConsumptionMsg, changes);
     }
   } catch (e) {
     // Do nothing
@@ -28,15 +30,21 @@ const logger = function(type, name, changes) {
 // 开启 state 支持
 function enableState(opts = {}) {
   const {
+    // 是否开启 debug 模式，支持3种参数： true, 'page', 'component'
     debug = false,
+
+    // 是否开启 component 支持
     component = true,
+
+    // 是否开启 page 支持
     page = true,
 
     // 数组删除操作： true 或者 false
-    overwriteArrayOnDeleted = true
+    overwriteArrayOnDeleted = true,
+
+    // 是否在 调用 setData 时自动同步 state
+    autoSync = true
   } = opts;
-
-
 
   // 解析变更内容
   function diffAndMergeChanges(state, obj) {
@@ -117,24 +125,43 @@ function enableState(opts = {}) {
     this.$$state = cloneDeep(this.data);
   }
 
+  // 给 setData 打补丁,
+  function patchSetData() {
+    this.__setData = this.setData;
+
+    this.setData = (obj, callback) => {
+      this.__setData(obj, function() {
+        // 如果开启自动同步，则在调用 setData 完成后，自动同步所有数据到 state 中
+        if (autoSync) initState.call(this);
+        if (typeof callback === 'function') return callback();
+      });
+    };
+  }
+
   // 设置状态函数
   function setState(obj, callback) {
     // 初始化状态
     if (!this.$$state) this.initState();
 
     // 计算增量更新
+    let time;
+    if (debug) time = +new Date();
+
+    // 计算变更
     let changes = diffAndMergeChanges(this.$$state, obj);
 
+    // 如果有变更，则触发更新
     if (changes) {
       // 开启调试模式
       if (debug) {
+        let timeConsumption = +new Date() - time;
         let type = this.__isPage ? 'Page:' : 'Component:';
-        if (debug === true) logger(type, this.route || this.is, changes);
-        if (debug === 'page' && this.__isPage) logger(type, this.route, changes);
-        if (debug === 'component' && this.__isComponent) logger(type, this.is, changes);
+        if (debug === true) logger(type, this.route || this.is, timeConsumption, changes);
+        if (debug === 'page' && this.__isPage) logger(type, this.route, timeConsumption, changes);
+        if (debug === 'component' && this.__isComponent) logger(type, this.is, timeConsumption, changes);
       }
 
-      this.setData(changes, callback);
+      this.__setData(changes, callback);
     } else {
       if (typeof callback === 'function') callback();
     }
@@ -144,16 +171,20 @@ function enableState(opts = {}) {
   if (page) {
     const $Page = Page;
     Page = function(obj = {}) {
+      obj.__isPage = true;
 
-      let originOnload = obj.onLoad || noop;
-      obj.onLoad = function () {
+      // 修改 onLoad 方法，页面载入时打补丁
+      let _onLoad = obj.onLoad || noop;
+      obj.onLoad = function() {
         initState.call(this);
-        return originOnload.apply(this, arguments);
+        patchSetData.call(this);
+        return _onLoad.apply(this, arguments);
       };
 
+      // 注入 initState 和 setState 方法
       obj.initState = function() { initState.call(this); };
       obj.setState = function() { setState.apply(this, arguments); };
-      obj.__isPage = true;
+
       return $Page(obj);
     };
   }
@@ -162,19 +193,28 @@ function enableState(opts = {}) {
   if (component) {
     const $Component = Component;
     Component = function(obj = {}) {
+      obj.lifetimes = obj.lifetimes || {};
+      obj.methods = obj.methods || {};
 
-      let originCreated = obj.created || noop;
-      obj.created = function () {
-        initState.call(this);
-        return originCreated.apply(this, arguments);
+      // 修改 created 方法，组件创建时打补丁
+      let _created = obj.lifetimes.created || obj.created || noop;
+      obj.lifetimes.created = obj.created = function() {
+        patchSetData.call(this);
+        this.__isComponent = true;
+        return _created.apply(this, arguments);
       };
 
-      obj.methods = obj.methods || {};
+      // 修改 attached 方法，组件挂载时初始化 state
+      let _attached = obj.lifetimes.attached || obj.attached || noop;
+      obj.lifetimes.attached = obj.attached = function() {
+        initState.call(this);
+        return _attached.apply(this, arguments);
+      };
+
+      // 注入 initState 和 setState 方法
       obj.methods.initState = function() { initState.call(this); };
       obj.methods.setState = function() { setState.apply(this, arguments); };
 
-      // 代替 true
-      obj.methods.__isComponent = function(){};
       return $Component(obj);
     };
   }
