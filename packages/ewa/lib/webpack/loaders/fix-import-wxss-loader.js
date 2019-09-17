4eda5de12d6cd1fa490890d454d82b474c618be1
lyfeyaj <lyfeyaj@gmail.com>
2019-09-17 18:54:23 +0800

    remove webpack related files from ewa

--- packages/ewa/lib/webpack/loaders/fix-import-wxss-loader.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function fixImportWxssLoader(content, map, meta) {
-  let re = /(@import\s*)url\(([^;)]+)\)(\s*;)/gi;
-  content = content.replace(re, '$1$2$3');
-  this.callback(null, content, map, meta);
-}
-
-module.exports = fixImportWxssLoader;
