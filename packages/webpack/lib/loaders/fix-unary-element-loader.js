afce4dc855b1cf8e45fb0e4f891a6b0a4a5fc368
lyfeyaj <lyfeyaj@gmail.com>
2020-05-09 18:22:12 +0800

    refactor

--- packages/webpack/lib/loaders/fix-unary-element-loader.js
+++ packages/webpack/lib/loaders/fix-unary-element-loader.js
@@ -1,11 +1,12 @@
 'use strict';
 
-// 修复一元元素在压缩wxss的时候解析错误的问题
-// 解决方案：重命名一元元素，等待压缩完成后，恢复之前的命名
-// 仅适用于 wxml
 const ELEMENT_MATCHER = /(<\/?)(area|base|basefont|br|col|embed|frame|hr|img|input|isindex|keygen|link|meta|param|source|track|wbr)( |>)/gi;
 const REPLACER = '___unary___';
 const REPLACER_MATCHER = /___unary___/g;
+
+// 修复一元元素在压缩wxss的时候解析错误的问题
+// 解决方案：重命名一元元素，等待压缩完成后，恢复之前的命名
+// 仅适用于 wxml
 function fixUnaryElementLoader(content, map, meta) {
   // 如果包含替换后的一元元素，则删除占位符
   if (content && content.indexOf(REPLACER) !== -1) {
