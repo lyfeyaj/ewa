d25696475748bdff6bbd719a893e128014843e13
lyfeyaj <lyfeyaj@gmail.com>
2021-03-17 13:15:42 +0800

    optimize queue and update deps

--- packages/ewa/lib/utils/Queue.js
+++ packages/ewa/lib/utils/Queue.js
@@ -1,65 +1,154 @@
 "use strict";
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-module.exports = function () {
-  function Queue(maxConcurrency) {
+var Queue = function () {
+  function Queue(concurrency) {
     _classCallCheck(this, Queue);
 
     this._queue = [];
-    this._maxConcurrency = maxConcurrency || 8;
-    this._currentConcurrency = 0;
+    this._concurrency = concurrency || 8;
+    this._pendingCount = 0;
   }
 
   _createClass(Queue, [{
-    key: "isEmpty",
-    value: function isEmpty() {
-      return !!this._queue.length;
+    key: "concurrency",
+    get: function get() {
+      return this._concurrency;
+    },
+    set: function set(num) {
+      this._concurrency = num;
+
+      while (this._canProcessNext()) {
+        this._processQueue();
+      }
     }
   }, {
-    key: "shift",
-    value: function shift() {
+    key: "size",
+    get: function get() {
+      return this._queue.length;
+    }
+  }, {
+    key: "isEmpty",
+    value: function isEmpty() {
+      return this.size === 0;
+    }
+  }, {
+    key: "_canProcessNext",
+    value: function _canProcessNext() {
+      return this.size > 0 && this._pendingCount < this._concurrency;
+    }
+  }, {
+    key: "_processQueue",
+    value: function _processQueue() {
       var _this = this;
 
-      if (this._currentConcurrency >= this._maxConcurrency) {
-        return Promise.resolve();
-      }
+      if (!this._canProcessNext()) return Promise.resolve();
 
       var next = this._queue.shift();
 
-      if (next) {
-        this._currentConcurrency++;
+      if (!next) return Promise.resolve();
+      this._pendingCount++;
 
-        var _handleNext = function _handleNext(e) {
-          if (e) console.log(e.message, e.stack);
-          _this._currentConcurrency--;
-          return _this.shift();
-        };
+      try {
+        return Promise.resolve(next()).then(function () {
+          return _this._handleNext();
+        }).catch(function (e) {
+          _this._handleError(e, next);
 
-        try {
-          return Promise.resolve(next()).then(function () {
-            return _handleNext();
-          }).catch(_handleNext);
-        } catch (e) {
-          return _handleNext(e);
-        }
-      } else {
-        return Promise.resolve();
+          return _this._handleNext();
+        });
+      } catch (e) {
+        this._handleError(e, next);
+
+        return this._handleNext();
       }
     }
   }, {
-    key: "push",
-    value: function push(item) {
-      this._queue.push(item);
+    key: "_handleNext",
+    value: function _handleNext() {
+      this._pendingCount--;
+      return this._processQueue();
+    }
+  }, {
+    key: "_handleError",
+    value: function () {
+      var _handleError2 = _asyncToGenerator(regeneratorRuntime.mark(function _callee(e, fn) {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                if (!(typeof this.onError === 'function')) {
+                  _context.next = 11;
+                  break;
+                }
 
-      this.shift();
+                _context.prev = 1;
+                _context.next = 4;
+                return Promise.resolve(this.onError(e, fn));
+
+              case 4:
+                _context.next = 9;
+                break;
+
+              case 6:
+                _context.prev = 6;
+                _context.t0 = _context["catch"](1);
+                console.log("Error: ".concat(_context.t0.message));
+
+              case 9:
+                _context.next = 12;
+                break;
+
+              case 11:
+                if (e) console.log("Error on executing ".concat(fn.name, ": ").concat(e.message));
+
+              case 12:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee, this, [[1, 6]]);
+      }));
+
+      function _handleError(_x, _x2) {
+        return _handleError2.apply(this, arguments);
+      }
+
+      return _handleError;
+    }()
+  }, {
+    key: "enqueue",
+    value: function enqueue(fn) {
+      if (typeof fn !== 'function') throw new Error("".concat(fn, " is not a valid function"));
+
+      this._queue.push(fn);
+
+      this._processQueue();
+
       return this;
+    }
+  }, {
+    key: "push",
+    value: function push(fn) {
+      return this.enqueue(fn);
+    }
+  }, {
+    key: "add",
+    value: function add(fn) {
+      return this.enqueue(fn);
     }
   }]);
 
   return Queue;
 }();
+
+module.exports = Queue;
--- packages/ewa/src/utils/Queue.js
+++ packages/ewa/src/utils/Queue.js
@@ -1,52 +1,111 @@
-module.exports = class Queue {
-  constructor(maxConcurrency) {
-    // 队列
+class Queue {
+  constructor(concurrency) {
+    // 队列, 用数组来代替队列
     this._queue = [];
 
     // 并发数
-    this._maxConcurrency = maxConcurrency || 8;
+    this._concurrency = concurrency || 8;
 
     // 当前并发数
-    this._currentConcurrency = 0;
+    this._pendingCount = 0;
   }
 
+  get concurrency() {
+    return this._concurrency;
+  }
+
+  set concurrency(num) {
+    this._concurrency = num;
+    while (this._canProcessNext()) this._processQueue();
+  }
+
+  // 队列长度
+  get size() {
+    return this._queue.length;
+  }
+
+  // 队列是否为空
   isEmpty() {
-    return !!this._queue.length;
+    return this.size === 0;
   }
 
-  shift() {
+  _canProcessNext() {
+    return this.size > 0 && this._pendingCount < this._concurrency;
+  }
+
+  // 处理队列
+  _processQueue() {
     // 当前并发数大于最大并发数时, 不做任何操作
-    if (this._currentConcurrency >= this._maxConcurrency) {
-      return Promise.resolve();
-    }
+    if (!this._canProcessNext()) return Promise.resolve();
 
     // 执行队列下一个请求
     let next = this._queue.shift();
 
-    if (next) {
-      this._currentConcurrency++;
+    // 如果队列为空，则什么都不做
+    if (!next) return Promise.resolve();
 
-      // 处理下一个请求
-      let _handleNext = (e) => {
-        // eslint-disable-next-line
-        if (e) console.log(e.message, e.stack);
-        this._currentConcurrency--;
-        return this.shift();
-      };
+    // pending +1
+    this._pendingCount++;
 
-      try {
-        return Promise.resolve(next()).then(() => _handleNext()).catch(_handleNext);
-      } catch (e) {
-        return _handleNext(e);
-      }
-    } else {
-      return Promise.resolve();
+    // 处理队列
+    try {
+      return Promise.resolve(next()).then(
+        () => this._handleNext()
+      ).catch((e) => {
+        this._handleError(e, next);
+        return this._handleNext();
+      });
+    } catch (e) {
+      this._handleError(e, next);
+      return this._handleNext();
     }
   }
 
-  push(item) {
-    this._queue.push(item);
-    this.shift();
+  _handleNext() {
+    // pending -1
+    this._pendingCount--;
+
+    // 继续处理队列
+    return this._processQueue();
+  }
+
+  async _handleError(e, fn) {
+    if (typeof this.onError === 'function') {
+      // 仅做错误信息打印，如需要做其他操作，可以覆盖此方法
+      try {
+        await Promise.resolve(this.onError(e, fn));
+      } catch (err) {
+        // eslint-disable-next-line
+        console.log(`Error: ${err.message}`);
+      }
+    } else {
+      // eslint-disable-next-line
+      if (e) console.log(`Error on executing ${fn.name}: ${e.message}`);
+    }
+  }
+
+  // 加入队列
+  enqueue(fn) {
+    if (typeof fn !== 'function') throw new Error(`${fn} is not a valid function`);
+
+    // 添加到队列
+    this._queue.push(fn);
+
+    // 尝试触发队列
+    this._processQueue();
+
     return this;
   }
-};
+
+  // enqueue 方法别名
+  push(fn) {
+    return this.enqueue(fn);
+  }
+
+  // enqueue 方法别名
+  add(fn) {
+    return this.enqueue(fn);
+  }
+}
+
+module.exports = Queue;
