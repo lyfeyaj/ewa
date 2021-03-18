197f5dbcc4719ee5886ad946b97870ff7eb0c708
xuechenfei <xuechenfei@bamaying.com>
2021-03-18 10:20:23 +0800

    百度小程序打包时,忽略指定路径替换为绝对路径

--- packages/webpack/lib/loaders/json-transform-loader.js
+++ packages/webpack/lib/loaders/json-transform-loader.js
@@ -6,10 +6,8 @@ const jsonParser = require('../parsers/jsonParser');
 module.exports = function jsonTransformLoader(content) {
   let { type, ENTRY_DIR } = this.query || {};
 
-  if (type !== 'weapp') {
-    let file = '/' + path.relative(ENTRY_DIR, this.resourcePath);
-    content = jsonParser(content, file, type);
-  }
+  let file = '/' + path.relative(ENTRY_DIR, this.resourcePath);
+  content = jsonParser(content, file, type);
 
   return content;
 };
--- packages/webpack/lib/parsers/jsonParser.js
+++ packages/webpack/lib/parsers/jsonParser.js
@@ -2,18 +2,35 @@
 
 const path = require('path');
 
+// 组件引用路径中，匹配到以下字符串，百度中不需要为绝对路径，其他平台直接删除此引用
+const EXCLUDE_URLS = ['dynamicLib://']
+
 // 百度小程序组件不支持相对路径，全部转换为绝对路径
 // 解析 usingComponents 并转换为绝对路径
 module.exports = function jsonParser(content, file, type) {
-  if (type !== 'swan') return content;
-
   let json = JSON.parse(content);
   let usingComponents = json.usingComponents || {};
   Object.keys(usingComponents).forEach(function (component) {
-    usingComponents[component] = path.resolve(path.dirname(file), usingComponents[component]);
+    if (requireDelete(usingComponents[component], type)) {
+      // 检测是否需要删除
+      delete usingComponents[component]
+    } else if (requireReplace(usingComponents[component], type)) {
+      // 检测是否需要替换为绝对路径
+      usingComponents[component] = path.resolve(path.dirname(file), usingComponents[component]);
+    }
   });
 
   json.usingComponents = usingComponents;
 
   return JSON.stringify(json, null, '  ');
 };
+
+// 组件引用url是否包含特殊字符串， 如果包含，则在非百度小程序删除此组件引用
+function requireDelete(url, type) {
+  return type !== 'swan' && EXCLUDE_URLS.some((item) => url.indexOf(item) > -1)
+}
+
+// 百度平台 匹配不到特殊字符串， 需要替换为绝对路径
+function requireReplace(url, type) {
+  return type === 'swan' && !EXCLUDE_URLS.some(item => url.indexOf(item) > -1)
+}
