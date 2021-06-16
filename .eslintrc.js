9ab678cbfc193d351319fc31c457fddb68c4fb62
lyfeyaj <lyfeyaj@gmail.com>
2021-06-17 00:15:27 +0800

    chore(.eslintrc.js): 修复 .eslintrc.js 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,57 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
+  "root": true,
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "globals": {
+    "wx": true,
+    "qq": true,
+    "tt": true,
+    "swan": true,
+    "my": true,
+    "App": true,
+    "Page": true,
+    "getApp": true,
+    "Component": true,
+    "Behavior": true,
+    "WeixinJSBridge": true,
+    "getCurrentPages": true
+  },
+  "requireConfigFile": false,
+  "parser": "@babel/eslint-parser",
+  "extends": ["eslint:recommended"],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true
     },
-    "globals": {
-        "wx": true,
-        "qq": true,
-        "tt": true,
-        "swan": true,
-        "my": true,
-        "App": true,
-        "Page": true,
-        "getApp": true,
-        "Component": true,
-        "Behavior": true,
-        "WeixinJSBridge": true,
-        "getCurrentPages": true
-    },
-    "parser": "@babel/eslint-parser",
-    "parserOptions": { "requireConfigFile": false },
-    "extends": ["eslint:recommended"],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true
-        },
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-unused-vars": [
-          1
-        ],
-        "no-console": [
-          1
-        ],
-        "indent": [
-            2,
-            2,
-            { "SwitchCase": 1, "MemberExpression": 1 }
-        ],
-        "linebreak-style": [
-            2,
-            "unix"
-        ],
-        "quotes": [
-            1,
-            "single"
-        ],
-        "semi": [
-            2,
-            "always"
-        ]
-    }
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-unused-vars": [
+      1
+    ],
+    "no-console": [
+      1
+    ],
+    "indent": [
+      2,
+      2,
+      { "SwitchCase": 1, "MemberExpression": 1 }
+    ],
+    "linebreak-style": [
+      2,
+      "unix"
+    ],
+    "quotes": [
+      1,
+      "single"
+    ],
+    "semi": [
+      2,
+      "always"
+    ]
+  }
 };
